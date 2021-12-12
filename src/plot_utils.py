6156c90ed99685aadec7e6babfba37b44174033b
Quentin18 <quentindeschamps18@orange.fr>
2021-12-12 05:56:50 +0100

    add plot_pie_parties

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -67,3 +67,34 @@ def plot_pie_top_speakers(
         fig.write_html(filename)
 
     return fig
+
+
+def plot_pie_parties(
+    df: pd.DataFrame,
+    title: str = 'Proportions of parties',
+    filename: str = None,
+):
+    """Plots the proportion of parties represented in a dataframe.
+
+    Args:
+        df (pd.DataFrame): dataframe of quotes.
+        title (str, optional): title. Defaults to 'Proportions of parties'.
+        filename (str, optional): filename to save the figure.
+        Defaults to None.
+    """
+
+    df_parties = df['party_name'].value_counts().reset_index()
+    df_parties.columns = ['party', 'counts']
+    df_parties['party'] = df_parties['party'].str.capitalize()
+
+    fig = px.pie(
+        df_parties, names='party', values='counts',
+        labels={'party': 'Party', 'counts': 'Number of quotations'},
+        title=title,
+    )
+    fig.update_traces(textinfo='percent+label')
+
+    if filename is not None:
+        fig.write_html(filename)
+
+    return fig
