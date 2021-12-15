68b4bace2bfa3046533e28fbf1bda4bb0b70fb3d
Quentin18 <quentindeschamps18@orange.fr>
2021-12-15 14:42:09 +0100

    update make_html_table_topics

--- src/table_utils.py
+++ src/table_utils.py
@@ -6,13 +6,22 @@ python3 table_utils.py > table.html
 """
 from tabulate import tabulate
 
-from constants import TOPICS_DICT
+from .constants import TOPICS_DICT
 
 
-def make_html_table_topics():
+def make_html_table_topics(topics_dict: dict = TOPICS_DICT) -> str:
+    """Makes a html table with the topics and the seed words.
+
+    Args:
+        topics_dict (dict, optional): topics and associated seed words.
+        Defaults to TOPICS_DICT.
+
+    Returns:
+        str: html table with topics and seed words.
+    """
     data = [
         [topic_name.capitalize(), ', '.join(seed_words)]
-        for topic_name, seed_words in TOPICS_DICT.items()
+        for topic_name, seed_words in topics_dict.items()
     ]
     return tabulate(data, headers=['Topic', 'Seed words'], tablefmt='html')
 
