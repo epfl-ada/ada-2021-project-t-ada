a64360a7bd30a67f4e65cc93ee415c44468cfb4e
Quentin18 <quentindeschamps18@orange.fr>
2021-12-15 16:52:14 +0100

    add pca functions

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -5,6 +5,8 @@ import nltk
 import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
 from scipy.stats import ttest_ind
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from tabulate import tabulate
 from tqdm import tqdm
 
@@ -89,3 +91,64 @@ def run_ttest(df: pd.DataFrame, alpha: float = 0.05) -> str:
         results.append([topic_name, statistic, pvalue, same_opinion])
 
     return tabulate(results, headers=headers, tablefmt='html', floatfmt='.4f')
+
+
+def create_df_avg_compound_score(df_topics: pd.DataFrame) -> pd.DataFrame:
+    """Creates the dataframe with the average of compound scores on each topic
+    for democrats and republicans from a daframe of topics.
+
+    Args:
+        df_topics (pd.DataFrame): dataframe of topics.
+
+    Returns:
+        pd.DataFrame: dataframe with average of compound scores.
+    """
+    # Create dataframe with only democrats and republicans
+    df_demo_repu = df_topics[
+        (df_topics['party_name'] == 'democratic party') | (
+            df_topics['party_name'] == 'republican party')
+    ]
+
+    # Average compound scores
+    df_avg = df_demo_repu.groupby(['label', 'party_name']).mean()
+
+    # Drop rows with all NaN and fill NaN by 0
+    df_avg = df_avg.dropna(how='all')
+
+    return df_avg
+
+
+def pca_analysis(
+    df: pd.DataFrame,
+    n_components: int = 2,
+    stardardize: bool = False,
+) -> tuple:
+    """Performs PCA on the dataframe and returns the PCA with the principal
+    components stored in a dataframe.
+
+    Args:
+        df (pd.DataFrame): dataframe.
+        n_components (int, optional): number of components. Defaults to 2.
+        stardardize (bool, optional): stardardize the scores.
+        Defaults to False.
+
+    Returns:
+        tuple: pca, dataframe with principal components
+    """
+    # Replace missing entries with 0
+    df_clean = df.fillna(0)
+
+    # Perform PCA
+    pca = PCA(n_components)
+    if stardardize:
+        scaler = StandardScaler()
+        data = pca.fit_transform(scaler.fit_transform(df_clean))
+    else:
+        data = pca.fit_transform(df_clean)
+
+    df_pca = pd.DataFrame(data, index=df_clean.index)
+
+    # Set columns names
+    df_pca.columns = [f'PC{i + 1}' for i in range(n_components)]
+
+    return pca, df_pca
